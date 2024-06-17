984fbe63c47b9525670ab4bc674b4dfadd87e8ba
Priyanka Anantha <priyankaananthashetty@gmail.com>
2024-06-17 15:18:59 +0530

    Refactor stock data fetching functionality

--- task1/task1app/views.py
+++ task1/task1app/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-
-# Creating views here.
-
+from django.views.decorators.csrf import csrf_exempt
 import requests
 
-def index(request):
+@csrf_exempt
+def fetch_stock_data(request):
     data = {}
-    if request.method == 'POST':
+    if request.method == "POST":
         ticker = request.POST.get('ticker')
         start_date = request.POST.get('start_date')
         end_date = request.POST.get('end_date')
         interval = request.POST.get('interval')
-        response = requests.get(f'http://localhost:8000/stocks/?ticker={ticker}&start_date={start_date}&end_date={end_date}&interval={interval}')
-        data = response.json()
-    return render(request, 'finance_app/index.html', {'data': data})
+        
+        # Replace this with your FastAPI endpoint
+        api_url = f"http://localhost:8000/stocks/?ticker={ticker}&start_date={start_date}&end_date={end_date}&interval={interval}"
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            data = response.json()
+        else:
+            data['error'] = 'Could not fetch data from API.'
+
+    return render(request, 'index.html', {'data': data})
